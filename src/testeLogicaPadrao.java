ba4dd9124cca0ad3db0d0fce3ca8b1f513ca719b
NickDevD <nick.developerdev@gmail.com>
2024-06-08 11:00:40 -0300

    testando o tipo double

---
+++ src/testeLogicaPadrao.java
@@ -0,0 +1,17 @@
+public class testeLogicaPadrao {
+
+    public static void main(String[] args) {
+        
+        //tipo double + variáveis 
+        double C, F, G, total;
+
+        C = 2;
+        F = 3;
+        G = 10;
+
+        total = (C * F) / G;
+
+        System.out.println(total);
+
+    }
+}
